9cfbce4a3213f76103c1743e413ac1bd9ed42d67
Uko Uwatt <uwattuko@gmail.com>
2024-07-27 00:47:00 +0100

    task 4

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -13,4 +13,26 @@ class MRUCache(BaseCaching):
         '''
         super().__init__()
         self.used_keys = []
+    
+    def put(self, key, item):
+        """
+            A methods that adds an item to the cache.
+            args: key - the key of the item to be added.
+                  item - the item to be added.
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+        if len(self.cache_data.keys()) > BaseCaching.MAX_ITEMS:
+            delete_key = self.most_recent_key
+            del self.cache_data[delete_key]
+            print(f'DISCARD: {delete_key}')
+        self.most_recent_key = key
 
+    def get(self, key):
+        """
+            A method that retrieves an item from the cache.
+            args: key - the key of the item to be retrieved.
+        """
+        if key is not None and self.cache_data.get(key) is not None:
+            self.most_recent_key = key
+        return self.cache_data.get(key, None)
